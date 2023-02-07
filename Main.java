f15973f02c2ab5717851808e48efc9023f563b82
HarshJaiswal18 <>
2023-02-08 01:00:29 +0530

    IDE change to VS code

--- Main.java
+++
@@ -1,8 +0,0 @@
-// import Patterns.*;
-class Main {
-  public static void main(String[] args) {
-    // Patterns/Test.main(args);
-    // Patterns.Test t=new Patterns.Test();
-    fullPyramid.main(args);
-  }
-}
